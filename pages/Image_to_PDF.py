d3e09766e6981c5979160f2d8e25e6fbc0e48dd5
fred-aiello <58234310+fred-aiello@users.noreply.github.com>
2023-06-19 19:13:06 +0200

    Update Image_to_PDF.py

--- pages/Image_to_PDF.py
+++ pages/Image_to_PDF.py
@@ -4,8 +4,8 @@ import streamlit as st
 uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
 for uploaded_file in uploaded_files:
     bytes_data = uploaded_file.read()
-    st.write("filename:", uploaded_file.name)
-    st.write("filename:", uploaded_file)
+    st.write("filename1:", uploaded_file.name)
+    st.write("filename2:", uploaded_file)
     st.write(bytes_data)
 
 file=r'/home/frederic/Documents/Rognac/Conseils Municipaux/Rognac Mag'
