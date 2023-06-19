ce585d8f3a6cde0c61d98de08adc2148c68a5520
fred-aiello <58234310+fred-aiello@users.noreply.github.com>
2023-06-19 19:11:33 +0200

    Update Image_to_PDF.py

--- pages/Image_to_PDF.py
+++ pages/Image_to_PDF.py
@@ -5,6 +5,7 @@ uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=Tru
 for uploaded_file in uploaded_files:
     bytes_data = uploaded_file.read()
     st.write("filename:", uploaded_file.name)
+    st.write("filename:", uploaded_file)
     st.write(bytes_data)
 
 file=r'/home/frederic/Documents/Rognac/Conseils Municipaux/Rognac Mag'
