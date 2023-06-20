6a98627c9ebfcd28f97f9c2f7a0b53a04e3f3a9d
fred-aiello <58234310+fred-aiello@users.noreply.github.com>
2023-06-20 14:59:12 +0200

    Update Image_to_PDF.py

--- pages/Image_to_PDF.py
+++ pages/Image_to_PDF.py
@@ -1,14 +1,31 @@
 # Receipt.png file to 'searchable' pdf file
 import streamlit as st
+import os
 
-uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
-for uploaded_file in uploaded_files:
-    bytes_data = uploaded_file.read()
-    st.write("filename1:", uploaded_file.name)
-    st.write("filename2:", uploaded_file)
-    st.write(bytes_data)
+# File selector
+def file_selector(folder_path='.'):
+    filenames = os.listdir(folder_path)
+    selected_filename = st.selectbox('Select a file', filenames)
+    return os.path.join(folder_path, selected_filename)
 
-file=r'/home/frederic/Documents/Rognac/Conseils Municipaux/Rognac Mag'
-PDF = pytesseract.image_to_pdf_or_hocr(file, extension='pdf')# export to searchable.pdf
-with open("searchable.pdf", "w+b") as f:
-    f.write(bytearray(PDF))
+filename = file_selector()
+st.write('You selected `%s`' % filename)
+
+path = folder_path
+
+all_files = []
+for (path,dirs,files) in os.walk(path):
+    
+    for file in sorted(files):
+        file = os.path.join(path, file)
+        print('file',file)
+        all_files.append(file)
+        pdf_writer = PyPDF2.PdfFileWriter()
+for file in all_files:
+    page = pytesseract.image_to_pdf_or_hocr(file, extension='pdf')
+    pdf = PyPDF2.PdfFileReader(io.BytesIO(page))
+    pdf_writer.addPage(pdf.getPage(0))
+    
+    
+with open(path + "/" + selected_filename + "2.pdf", "wb") as f:
+    pdf_writer.write(f)
