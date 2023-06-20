1b67ccf31adbba9ba1385550d9938e1cb2f94fbd
fred-aiello <58234310+fred-aiello@users.noreply.github.com>
2023-06-20 16:04:11 +0200

    Update Image_to_PDF.py

--- pages/Image_to_PDF.py
+++ pages/Image_to_PDF.py
@@ -6,6 +6,7 @@ uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=Tru
 for uploaded_file in uploaded_files:
     bytes_data = uploaded_file.read()
     st.write("filename:", uploaded_file.name)
+    st.write("Path_filename:", uploaded_file)
     #st.write(bytes_data)
 
 ## first file in current dir (with full path)
