be5a9761021b33fcb9410d92cdf224ac562ee0c4
fred-aiello <58234310+fred-aiello@users.noreply.github.com>
2023-06-26 18:23:09 +0200

    Update Scanned_PDF.py

--- pages/Scanned_PDF.py
+++ pages/Scanned_PDF.py
@@ -5,7 +5,7 @@ from PIL import Image
 import pandas as pd
 from pdf2image import convert_from_path
 import PyPDF2
-import os
+#import os
 from functions.test import calcul
 
 st.write("# Convert a scanned PDF document into a searchable PDF file 👋")
